a5c3f5094683aa90f5e71df1830bea30800b62b9
Yaroslav Karpovets <yaroslav.karpovets@gettyimages.com>
2020-11-14 23:18:32 +0200

    Launch concurrent loading in Displathers.default coroutine context

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,16 +4,18 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    val repos = service.getOrgRepos(req.org)
-            .also { logRepos(req, it) }
-            .body() ?: listOf()
+    withContext(Dispatchers.Default) {
+        val repos = service.getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .body() ?: listOf()
 
-    repos.map { repo ->
-        async {
-            service
-                    .getRepoContributors(req.org, repo.name)
-                    .also { logUsers(repo, it) }
-                    .bodyList()
-        }
-    }.awaitAll().flatten().aggregate()
+        repos.map { repo ->
+            async {
+                service
+                        .getRepoContributors(req.org, repo.name)
+                        .also { logUsers(repo, it) }
+                        .bodyList()
+            }
+        }.awaitAll().flatten().aggregate()
+    }
 }
