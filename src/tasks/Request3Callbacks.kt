f22b314d827444ed16a8c8d8a3877d92d6547b5f
Yaroslav Karpovets <yaroslav.karpovets@gettyimages.com>
2020-11-14 22:56:17 +0200

    Fix issue with callbacks based users loading

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,33 +5,44 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
-        val allUsers = mutableListOf<User>()
+        val allUsers = ConcurrentLinkedQueue<User>()
+        val countDownLatch = CountDownLatch(repos.size)
         for (repo in repos) {
-            service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
+            service.getRepoContributorsCall(req.org, repo.name).onComplete({ responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
-            }
+                countDownLatch.countDown()
+            }, {
+                log.error("Call failed", it)
+                countDownLatch.countDown()
+            })
         }
-        // TODO: Why this code doesn't work? How to fix that?
-        updateResults(allUsers.aggregate())
+        countDownLatch.await()
+        updateResults(allUsers.toList().aggregate())
     }
 }
 
-inline fun <T> Call<T>.onResponse(crossinline callback: (Response<T>) -> Unit) {
+inline fun <T> Call<T>.onResponse(crossinline responseCallback: (Response<T>) -> Unit) {
+    onComplete({ responseCallback(it)}, { log.error("Call failed", it)})
+}
+
+inline fun <T> Call<T>.onComplete(crossinline responseCallback: (Response<T>) -> Unit, crossinline failedCallback: (Throwable) -> Unit) {
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
-            callback(response)
+            responseCallback(response)
         }
 
         override fun onFailure(call: Call<T>, t: Throwable) {
-            log.error("Call failed", t)
+            failedCallback(t)
         }
     })
 }
