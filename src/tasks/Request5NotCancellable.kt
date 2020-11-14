0ffc0c78da9c4af4419bbbf9e02be3a6503a3b7a
Yaroslav Karpovets <yaroslav.karpovets@gettyimages.com>
2020-11-14 23:49:02 +0200

    Implement loading with progress update

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,18 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    return GlobalScope.async {
+        val repos = service.getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .body() ?: listOf()
+
+        repos.map { repo ->
+            GlobalScope.async {
+                service
+                        .getRepoContributors(req.org, repo.name)
+                        .also { logUsers(repo, it) }
+                        .bodyList()
+            }
+        }.awaitAll().flatten().aggregate()
+    }.await()
 }
--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,17 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service.getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+    var loadedUsers = emptyList<User>()
+    repos.forEachIndexed { index, repo ->
+        val users = service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        loadedUsers = (loadedUsers + users).aggregate()
+        updateResults(loadedUsers, index == repos.lastIndex)
+    }
 }
