e60e8faf86f8e68e7374450413b5f72f8b05ee45
Yaroslav Karpovets <yaroslav.karpovets@gettyimages.com>
2020-11-15 21:53:38 +0200

    Implement concurrent loading with channel and progress update

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,26 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service.getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .body() ?: listOf()
+
+        val channel = Channel<List<User>>(repos.size)
+        repos.map { repo ->
+            launch {
+                channel.send(
+                        service
+                                .getRepoContributors(req.org, repo.name)
+                                .also { logUsers(repo, it) }
+                                .bodyList()
+                )
+            }
+        }
+        var loadedUsers = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            loadedUsers = (loadedUsers + users).aggregate()
+            updateResults(loadedUsers, it == repos.lastIndex)
+        }
     }
 }
